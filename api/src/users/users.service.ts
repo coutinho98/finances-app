013c68bf4a402b7c2416e187319ad9bf13264bb1
Mateus Couto <mateuscouto98@outlook.com>
2024-10-16 22:35:25 -0300

    feature: Unique constraint failed on the fields: {'email'}

--- api/src/users/users.service.ts
+++ api/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { PrismaService } from 'src/database/prisma.service';
 
@@ -7,6 +7,16 @@ export class UsersService {
   constructor(private readonly prismaService: PrismaService) {}
 
   async create(createUserDto: CreateUserDto) {
+    const { name, email, password } = createUserDto;
+
+    const emailTaken = await this.prismaService.user.findUnique({
+      where: { email },
+    });
+
+    if (emailTaken) {
+      throw new ConflictException('This email is already in use.');
+    }
+
     const user = await this.prismaService.user.create({
       data: {
         name: createUserDto.name,
