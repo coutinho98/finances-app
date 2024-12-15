29512b936de2b9d3e2bacd87c473a8b3addf4bb6
Mateus Couto <mateuscouto98@outlook.com>
2024-12-15 18:15:41 -0300

    created and configuration users repositories

--- api/src/modules/users/users.service.ts
+++ api/src/modules/users/users.service.ts
@@ -1,23 +1,24 @@
 import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { PrismaService } from 'src/database/prisma.service';
+import { UsersRepository } from 'src/shared/database/repositories/users.repositories';
 
 @Injectable()
 export class UsersService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(private readonly usersRepo: UsersRepository) {}
 
   async create(createUserDto: CreateUserDto) {
     const { name, email, password } = createUserDto;
 
-    const emailTaken = await this.prismaService.user.findUnique({
+    const emailTaken = await this.usersRepo.findUnique({
       where: { email },
+      select: { id: true },
     });
 
     if (emailTaken) {
       throw new ConflictException('This email is already in use.');
     }
 
-    const user = await this.prismaService.user.create({
+    const user = await this.usersRepo.create({
       data: {
         name: createUserDto.name,
         email: createUserDto.email,
