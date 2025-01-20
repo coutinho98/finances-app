affdbfa2780e3dcf1130e225878704dfbda71cee
Mateus Couto <mateuscouto98@outlook.com>
2025-01-20 08:55:39 -0300

    fk u yarn start...

--- api/src/modules/auth/auth.service.ts
+++ api/src/modules/auth/auth.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { AuthenticateDto } from './dto/authenticate.dto';
+import { UsersRepository } from 'src/shared/database/repositories/users.repositories';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly usersRepo: UsersRepository) {}
+
+  async authenticate(authenticateDto: AuthenticateDto) {
+    const { email } = authenticateDto;
+
+    const users = await this.usersRepo.findUnique({
+      where: { email },
+    });
+
+    return { users };
+  }
+}
